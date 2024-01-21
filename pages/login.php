0fe8b2f330bb3c5644e0a58c9717c265eb7057be
marcosgzMSMK <157277365+marcosgzMSMK@users.noreply.github.com>
2024-01-21 10:38:14 +0100

    Update login.php - Implementación del Framework de Boostrap

--- pages/login.php
+++ pages/login.php
@@ -40,24 +40,52 @@ $mysqli->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión - Haccing</title>
     <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> <!-- Framework de Boostrap -->
     <link rel="icon" href="../img/HacckingGadget-icon.ico">
 </head>
+<style>
+    body {
+    background-color: #131313; /* Color de fondo */
+    color: white; /* Color del texto */
+    }
+
+    .login{
+        background-color: #292929;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    form {
+    max-width: 300px; /* Ancho máximo del formulario */
+    margin: 0 auto; /* Centrar el formulario en la página */
+    }
+
+    .form-group {
+        margin-bottom: 15px; /* Espaciado entre grupos de formularios */
+    }
+
+    .form-control {
+        width: 100%; 
+    }
+</style>
 <!-- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
 <!--Estructura del formulario de Inicio de Sesión-->
 <body>
-    <main>  
-        <section class="login">
-            <h1>Iniciar Sesión</h1>
+    <main class="container">
+        <section class="login mt-5">
+            <h1 class="mb-4">Iniciar Sesión</h1>
             <form action="login.php" method="post">
-                <label for="username">Nombre de Usuario:</label>
-                <input type="text" id="username" name="username" required>
-
-                <label for="password">Contraseña:</label>
-                <input type="password" id="password" name="password" required>
-
-                <input type="submit" value="Iniciar Sesión">
+                <div class="form-group">
+                    <label for="username">Nombre de Usuario:</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
             </form>
-            <p>¿Aún no te has registrado? <a href="signup.php">Regístrate aquí</a></p> <!---Mostrar al usuario que puede ser redirigido para registrarse-->
+            <p class="mt-3">¿Aún no te has registrado? <a href="signup.php">Regístrate aquí</a></p>
         </section>
     </main>
 </body>
